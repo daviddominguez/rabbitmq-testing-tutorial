395b1858913fd3fba30f7c7d568917a3f1075d65
David Dominguez <david.dominguez@amplia.es>
2016-07-22 10:04:42 +0200

    feat(hello-world-rabbitmq): add hello world implementation
    As explained in https://www.rabbitmq.com/tutorials/tutorial-one-java.html

---
+++ src/main/java/es/amplia/utils/rabbitmq/helloWorld/Recv.java
@@ -0,0 +1,34 @@
+package es.amplia.utils.rabbitmq.helloWorld;
+
+import com.rabbitmq.client.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Recv.class);
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        LOGGER.debug("Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                LOGGER.debug("Received '{}' message", message);
+            }
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
---
+++ src/main/java/es/amplia/utils/rabbitmq/helloWorld/Send.java
@@ -0,0 +1,31 @@
+package es.amplia.utils.rabbitmq.helloWorld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Send {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Send.class);
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        LOGGER.debug("Sent '{}' message");
+
+        channel.close();
+        connection.close();
+    }
+}
